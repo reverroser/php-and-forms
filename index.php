b03ad8b542df94f317d8e02312a1a02eb40244f9
Roser Reverte <reverroser@gmail.com>
2019-11-13 09:41:52 +0100

    multiple file php

--- index.php
+++ index.php
@@ -9,44 +9,44 @@
 </head>
 
 <body>
-<!-- This form sends the information that the user introduces and is sent 
+    <!-- This form sends the information that the user introduces and is sent 
 through POST method to the server. When the user clicks submit, the user
 is sent to the welcome php file and then information is displayed through echoing 
 all the variables. -->
-<div class="form-container">
-<h1>Form with POST method</h1>
-<form action="welcome_post.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-</div>
+    <div class="form-container">
+        <h1>Form with POST method</h1>
+        <form action="welcome_post.php" method="post">
+            Name: <input type="text" name="name"><br>
+            E-mail: <input type="text" name="email"><br>
+            <input type="submit">
+        </form>
+    </div>
 
-<!-- This form sends the information that the user introduces and is sent 
+    <!-- This form sends the information that the user introduces and is sent 
 through GET method to the server. When the user clicks submit, the user
 is sent to the welcome php file and then information is displayed through echoing 
 all the variables. -->
-<div class="form-container">
-<h1>Form with GET method</h1>
-<form action="welcome_get.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-</div>
+    <div class="form-container">
+        <h1>Form with GET method</h1>
+        <form action="welcome_get.php" method="get">
+            Name: <input type="text" name="name"><br>
+            E-mail: <input type="text" name="email"><br>
+            <input type="submit">
+        </form>
+    </div>
 
 
-<!-- This form allows the user to upload multiple files to a server, and we are 
+    <!-- This form allows the user to upload multiple files to a server, and we are 
 using the POST method to send data -->
-<div class="form-container">
-<h1>Multiple files uploading form</h1>
-<!--The 'multipart/form-data' value is required for uploading files in forms.-->
-<form method="post" enctype="multipart/form-data">
-<input type="file" name="files[]" multiple />
-<input type="submit" value="Upload File" name="submit" />
-</form>
-
-<script src="upload.js"></script>
+    <div class="form-container">
+        <h1>Select and upload multiple files to the server</h1>
+        <!--The 'multipart/form-data' value is required for uploading files in forms.-->
+        <form action="file_upload.php" method="POST" enctype="multiple/form-data">
+        <p> Select files to upload </p>    
+        <input type="file" name="files[]" multiple>
+            <input type="submit" name="submit" value="Upload">
+        </form>
+    </div>
 </body>
 
 </html>
