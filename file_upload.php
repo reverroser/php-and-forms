81e4c65e53ad49d612ff51eeb432938f776dbe5d
Roser Reverte <reverroser@gmail.com>
2019-11-14 11:36:02 +0100

    creates folder with today's date and stores uploaded fliles

--- file_upload.php
+++ file_upload.php
@@ -1,49 +1,52 @@
 <?php
 if (isset($_POST['submit'])) {
     $file = $_FILES['file'];
-// print_r($file);
 
 $fileName = $_FILES['file']['name'];
 $fileTmpName = $_FILES['file']['tmp_name'];
 $fileSize = $_FILES['file']['size'];
 $fileError = $_FILES['file']['error'];
 $fileType = $_FILES['file']['type'];
-// to take appart a string that has the name.type, so we can check the type
-$fileExt = explode('.', $fileName);
-// after exploding the string, we get an array. To get the last item of the array
-// (that is going to be the type) we want to put it in lower case:
-$fileActualExt = strtolower(end($fileExt));
 
-// this are the files extentions we want to allow to be uploaded
+for ($i=0; $i < sizeof($_FILES); $i++) {
+    // CHECK THE FORMAT OF THE FILES:
+    // to take appart a string that has the name.type, so we can check the type
+    $fileExt = explode('.', $fileName);
+    // after exploding the string, we get an array. To get the last item of the array
+    // (that is going to be the type) we want to put it in lower case:
+    $fileActualExt = strtolower(end($fileExt));
+    // this are the files extentions we want to allow to be uploaded
+    $allowed = array('jpg', 'png', 'txt', 'pdf');
+    //let's check if any of the extentions allowed are inside the $fileActualExt:
+     if (in_array($fileActualExt, $allowed)) {
+            //check if there hasn't been any error
+            if ($fileError === 0) {
+                //CHECK IF THE SIZE IS CORRECT:
+                if ($fileSize < 4*1024*1024) {
+                    // Create a new directory with the date of today
+                    $dateFile = date("d-m-Y"."/");
+                    $directoryStorage = "uploads/".$dateFile;
+                    $finalDirectory = $directoryStorage.$fileName;
+                    // Check if it doesn't exists, then create one or it already exists, then save files there.
+                    if(!file_exists($finalDirectory)) {
+                        mkdir('uploads/'.date("d-m-Y"),0777);
+                    }else{
+                     move_uploaded_file($fileTmpName,$finalDirectory);
+                    }
+                    //create a function that moves the file from the temporay location ($fileTmpName)to the location we want to 
 
-$allowed = array('jpg', 'png', 'txt', 'pdf');
+                    //be located to ($fileDestination)
+                    header("Location: index.php?uploadsucess");
+                }else {
+                    echo "Your file was too heavy";
+                }
 
-//let's check if any of the extentions allowed are inside the $fileActualExt:
-    if (in_array($fileActualExt, $allowed)) {
-        //check if there hasn't been any error
-        if ($fileError === 0) {
-            //check if the size is correct:
-            if ($fileSize < 4000000) {
-                // Prevent the file names to be repeated:
-                // this uniqid gives a unic id based on thereal time microseconds that the file has been uploaded
-                // and we concatenate the file extention to the file name given
-                $fileNameNew = uniqid('', true).".".$fileActualExt;
-                //we say where we want to upload the file, the destination. With the new name given.
-                $fileDestination = 'uploads/'.$fileNameNew;
-                //create a function that moves the file from the temporay location ($fileTmpName)to the location we want to 
-                //be located to ($fileDestination)
-                move_uploaded_file($fileTmpName, $fileDestination);
-                header("Location: index.php?uploadsucess");
             }else {
-                echo "Your file was too heavy";
+            echo "There was an error uploading your file.";
             }
-
         }else {
-        echo "There was an error uploading your file";
-    }
-    }else {
-        echo "You cannot upload files of this type";
-    }
-
-}
+                    echo "You cannot upload files of this type";
+            }
+        }
+};
 ?>
