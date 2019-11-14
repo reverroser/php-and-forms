f7a819eac245d2d934478318813659ba425e20e5
Roser Reverte <reverroser@gmail.com>
2019-11-14 14:03:12 +0100

    comments changed

--- file_upload.php
+++ file_upload.php
@@ -10,20 +10,21 @@ $fileType = $_FILES['file']['type'];
 
 for ($i=0; $i < sizeof($_FILES); $i++) {
     // CHECK THE FORMAT OF THE FILES:
-    // to take appart a string that has the name.type, so we can check the type
+    // To take appart a string that has the name.type, so we can check the type
     $fileExt = explode('.', $fileName);
-    // after exploding the string, we get an array. To get the last item of the array
-    // (that is going to be the type) we want to put it in lower case:
+    // After exploding the string, we get an array. To get the last item of the array
+    // (type) we want to put it in lower case:
     $fileActualExt = strtolower(end($fileExt));
-    // this are the files extentions we want to allow to be uploaded
+    // Files extentions we want to allow to be uploaded
     $allowed = array('jpg', 'png', 'txt', 'pdf');
-    //let's check if any of the extentions allowed are inside the $fileActualExt:
+    // Check if any of the extentions allowed are inside the $fileActualExt:
      if (in_array($fileActualExt, $allowed)) {
             //check if there hasn't been any error
             if ($fileError === 0) {
                 //CHECK IF THE SIZE IS CORRECT:
                 if ($fileSize < 4*1024*1024) {
                     // Create a new directory with the date of today
+                    // IN MAC, MANUALLY GIVE PERMISIONS.
                     $dateFile = date("d-m-Y"."/");
                     $directoryStorage = "uploads/".$dateFile;
                     $finalDirectory = $directoryStorage.$fileName;
@@ -33,19 +34,21 @@ for ($i=0; $i < sizeof($_FILES); $i++) {
                     }else{
                      move_uploaded_file($fileTmpName,$finalDirectory);
                     }
-                    //create a function that moves the file from the temporay location ($fileTmpName)to the location we want to 
+                    if(!file_exists($fileName)) {
 
-                    //be located to ($fileDestination)
+                    }else{
+
+                    }
+                    //if loading sucessfull:
                     header("Location: index.php?uploadsucess");
                 }else {
                     echo "Your file was too heavy";
                 }
-
             }else {
             echo "There was an error uploading your file.";
             }
         }else {
-                    echo "You cannot upload files of this type";
+            echo "You cannot upload files of this type";
             }
         }
 };
