5d56e8ff3ee22bd3f5f5d7826eadbe3e2686bdd8
Roser Reverte <reverroser@gmail.com>
2019-11-18 07:40:55 +0100

    styles and titles final commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Alata&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
+    <title>PHP & FORMS</title>
 </head>
 
 <body>
--- welcome_post.php
+++ welcome_post.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Alata&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+    <title>Welcome with POST</title>
 </head>
 <body>
 Welcome <?php echo $_POST["name"]; ?><br>
